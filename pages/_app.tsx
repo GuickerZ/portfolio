6bd9b818c5a942f0fa34b99290eddf85f4980517
GuickerCheats <80132563+GuickerCheats@users.noreply.github.com>
2023-10-08 06:53:30 -0300

    aaa2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
-import { WindowProvider } from "@/context/windowContext";
+import { WindowProvider } from "@/contexts/windowContext";
 import "@/styles/globals.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
