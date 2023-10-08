e9ca028230ef1297431b6db6615a830eb6e4333e
GuickerCheats <80132563+GuickerCheats@users.noreply.github.com>
2023-10-08 16:46:18 -0300

    aaa4

--- pages/anselmo.tsx
+++ pages/anselmo.tsx
@@ -8,12 +8,19 @@ const Home: NextPage = () => {
 		<div className="w-screen h-screen flex items-center justify-center p-5 bg-zinc-900">
 			<WindowLayout>
 				<div className="flex flex-col gap-2 items-center justify-center w-full h-full">
-					<h2 className="text-center text-xl text-green-500 select-none">Anselmo te ama S2</h2>
+					<h2 className="text-center text-xl text-white-500 select-none">ANSELMO & MARILHA 😍</h2>
+					<Image
+						src="/anselmo.png"
+						alt="😍"
+						width={500}
+						height={500}
+						className="rounded-xl mb-5"
+					/>
 					<Image
 						src="/352796922_1710194966089306_8989713870459742730_n.png"
 						alt="😍"
-						width={600}
-						height={600}
+						width={500}
+						height={500}
 						className="rounded-xl mb-5"
 					/>
 				</div>
